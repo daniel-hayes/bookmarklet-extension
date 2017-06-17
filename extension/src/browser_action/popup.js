d3bfac436507070f1221e663ec1cabd8a625ba6e
Daniel Hayes <dan.rdhayes@gmail.com>
2017-06-17 11:52:08 -0400

    began adding filter

--- extension/src/browser_action/popup.js
+++ extension/src/browser_action/popup.js
@@ -1,3 +1,8 @@
+// make this a class/object
+let buttons = [];
+let body;
+let bookmarkletContainer;
+
 const click = (e) => {
   e.preventDefault();
   const source = e.target.getAttribute('data-source');
@@ -13,21 +18,42 @@ const click = (e) => {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+const filter = (e) => {
+  const inputValue = e.target.value.toLowerCase();
+  const buttonList = buttons.filter(button => {
+    if (button.innerText.toLowerCase().indexOf(inputValue) !== -1) {
+      return button;
+    }
+  });
+
+  // probably a better way to do this
+  if (buttonList.length === 0) {
+    bookmarkletContainer.innerHTML = '';
+  } else {
+    buttonList.forEach(bookmarklet => bookmarkletContainer.appendChild(bookmarklet));
+  }
+}
+
+// TODO is it better to load entire repos using config file, or set these in the options page? or both?
+document.addEventListener('DOMContentLoaded', () => {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "https://rawgit.com/daniel-hayes/bookmarklet-extension/master/bookmarklet.config.json", true);
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = () => {
     if (xhr.readyState == 4) {
       const bookmarklets = JSON.parse(xhr.responseText);
-      const body = document.getElementById('popup');
+      body = document.getElementById('popup');
+      bookmarkletContainer = document.getElementById('bookmarklets');
+
+      bookmarkletContainer.innerHTML = '';
       bookmarklets.forEach(bookmarklet => {
-        body.innerHTML += `<button data-source="https://rawgit.com/daniel-hayes/bookmarklet-extension/master${bookmarklet.path}">${bookmarklet.name}</button>`;
+        bookmarkletContainer.innerHTML += `<button data-source="https://rawgit.com/daniel-hayes/bookmarklet-extension/master${bookmarklet.path}">${bookmarklet.name}</button>`;
       });
 
-      const buttons = document.querySelectorAll('button');
-      for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', click);
-      }
+      const input = document.getElementById('filter');
+      buttons = [...document.querySelectorAll('button')]
+
+      input.addEventListener('keyup', filter);
+      body.addEventListener('click', click);
     }
   }
   xhr.send();
